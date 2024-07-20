8b8063a0e9101122c320a9ff3f7025981666cc04
naghamtareq18 <m.ghiad91@gmail.com>
2024-07-20 06:28:00 +0300

    ee

--- tasks/reactjs/session3/clean-blog/src/pages/Contact.jsx
+++ tasks/reactjs/session3/clean-blog/src/pages/Contact.jsx
@@ -18,7 +18,45 @@ export const ContactPage = () => {
 					Want to get in touch? Fill out the form below to send me a message and
 					I will get back to you as soon as possible!
 				</p>
-				<form action=""></form>
+				<form action="">
+					<div className="form-floating mb-3">
+						<input
+							type="text"
+							className="form-control"
+							id="floatingInput"
+							placeholder="Name"
+						/>
+						<label htmlFor="floatingInput">Name</label>
+					</div>
+					<div className="form-floating mb-3">
+						<input
+							type="Email address"
+							className="form-control"
+							id="floatingInput"
+							placeholder="name@example.com"
+						/>
+						<label htmlFor="floatingInput">Email address</label>
+					</div>
+					<div className="form-floating mb-3">
+						<input
+							type="text"
+							className="form-control"
+							id="floatingInput"
+							placeholder="012345....."
+						/>
+						<label htmlFor="floatingInput">phone number</label>
+					</div>
+					<div className="form-floating mb-3">
+						<textarea
+							className="form-control"
+							placeholder="Leave a comment here"
+							id="floatingTextarea2Disabled"
+							style="height: 100px"
+						></textarea>
+						<label htmlFor="floatingInput">phone number</label>
+					</div>
+					<button className="btn btn-primary px-3">SEND</button>
+				</form>
 			</section>
 		</div>
 	);
